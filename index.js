cca1ed6faa53be8a5d489404576d911154f7ecb1
Leissan Khan <leissan.khan@gmail.com>
2022-01-15 20:39:03 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+
+}
+destructivelyAppendCat("Ralph")
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+
+}
+destructivelyPrependCat("Bob")
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+destructivelyRemoveLastCat()
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+destructivelyRemoveFirstCat()
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function appendCat(name) {
+    const newcats=[...cats];
+      newcats.push(name);
+    return newcats
+  }
+  appendCat("Broom")
+
+  beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+  
+function prependCat(name) {
+    const newcats=[...cats];
+      newcats.unshift(name);
+    return newcats
+  }
+  prependCat("Arnold")
+
+  beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function removeLastCat(name) {
+    const newcats=[...cats];
+      newcats.pop(name);
+    return newcats
+  }
+  removeLastCat("Arnold")
+
+  beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function removeFirstCat(name) {
+    const newcats=[...cats];
+      newcats.shift(name);
+    return newcats
+  }
+  removeFirstCat("Arnold")
